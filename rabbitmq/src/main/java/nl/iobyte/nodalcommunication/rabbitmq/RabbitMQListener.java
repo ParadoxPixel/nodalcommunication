4cd99fc7b53b72233de927bf83e1b2146d9698d4
thomas <thomas.weert@icloud.com>
2021-12-12 01:23:14 +0100

    Removed unnecessary complexity

--- rabbitmq/src/main/java/nl/iobyte/nodalcommunication/rabbitmq/RabbitMQListener.java
+++ rabbitmq/src/main/java/nl/iobyte/nodalcommunication/rabbitmq/RabbitMQListener.java
@@ -2,7 +2,6 @@ package nl.iobyte.nodalcommunication.rabbitmq;
 
 import nl.iobyte.nodalcommunication.generic.Node;
 import nl.iobyte.nodalcommunication.generic.interfaces.packet.IPacket;
-import nl.iobyte.nodalcommunication.generic.namespace.NamespaceComparator;
 import nl.iobyte.rabbitwrapper.thirdparty.com.rabbitmq.client.DeliverCallback;
 import nl.iobyte.rabbitwrapper.thirdparty.com.rabbitmq.client.Delivery;
 import java.util.concurrent.ForkJoinPool;
@@ -10,11 +9,9 @@ import java.util.concurrent.ForkJoinPool;
 public class RabbitMQListener implements DeliverCallback {
 
     private final Node node;
-    private final NamespaceComparator comparator;
 
     public RabbitMQListener(Node node) {
         this.node = node;
-        comparator = new NamespaceComparator(node.getId());
     }
 
     /**
@@ -23,36 +20,15 @@ public class RabbitMQListener implements DeliverCallback {
      * @param delivery Delivery
      */
     public void handle(String tag, Delivery delivery) {
-        handle(
-                delivery.getEnvelope().getExchange(),
-                delivery.getEnvelope().getRoutingKey(),
-                delivery.getBody()
+        IPacket<?> packet = node.getFactory().getSerializer().deserialize(
+                delivery.getBody(),
+                node.getFactory().getType()
         );
-    }
-
-    /**
-     * Handle incoming data
-     * @param channel String
-     * @param message String
-     */
-    public void handle(String channel, String node_id, byte[] message) {
-        if(comparator.compare(node_id) < 0)
-            return;
-
-        if(message[0] != '{')
+        if(packet == null)
             return;
 
         //Run async
-        ForkJoinPool.commonPool().execute(() -> {
-            IPacket<?> packet = node.getFactory().getSerializer().deserialize(
-                    message,
-                    node.getFactory().getType()
-            );
-            if(packet == null)
-                return;
-
-            node.handle(packet);
-        });
+        ForkJoinPool.commonPool().execute(() -> node.handle(packet));
     }
 
 }
